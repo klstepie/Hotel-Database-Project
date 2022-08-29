ea8bf18b6600868d5d1e4a6733e70cc15207155f
klstepie <107317111+klstepie@users.noreply.github.com>
2022-08-29 23:41:29 +0200

    Update HotelDatabase.sql

--- HotelDatabase.sql
+++ HotelDatabase.sql
@@ -53,13 +53,15 @@ ADD CHECK  (([PESEL] LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-
 -- Dumping data 
 
 INSERT INTO Employees 
-VALUES (1, N'Jan', N'Kowalski', N'Parkingowa/y', N'ul. Motylkowa 2', N'Wroc³aw', N'00-000', N'Polska', N'876321123   ', N'11111111111', N'Employed'),
-(2, N'Maria', N'Nowak', N'Pokojowa/y', N'ul. Ró¿ana 2', N'Rzeszów', N'00-001', N'Polska', N'876321111   ', N'22222222222', N'Employed'),
-(3, N'Monika', N'Wichura', N'Pokojowa/y', N'ul. Bratkowa 2', N'Kraków', N'00-002', N'Polska', N'876321122   ', N'33333333333', N'Employed'),
-(4, N'£ukasz', N'Biegun', N'Ratownik', N'ul. Irysowa 2', N'Hel', N'00-003', N'Polska', N'876321133   ', N'44444444444', N'Employed'),
-(5, N'Józef', N'Stachowski', N'Masa¿ysta/ka', N'ul. Rzepakowa 2', N'Wroc³aw', N'00-004', N'Polska', N'876321144   ', N'55555555555', N'Employed'),
-(6, N'Anna', N'WoŸniak', N'Kierownik recepcji', N'ul. Ziemniaczana 2', N'Wroc³aw', N'00-005', N'Polska', N'876321155   ', N'66666666666', N'Employed'),
-(7, N'Krzysztof', N'P¹k', N'Recepcjonista/ka', N'ul. Marchewkowa 2', N'Wroc³aw', N'00-006', N'Polska', N'876321166   ', N'77777777777', N'Employed')
+VALUES (1, N'Jan', N'Kowalski', N'Car park attendant', N'ul. Motylkowa 2', N'Wroclaw', N'00-000', N'Polska', N'876321123   ', N'11111111111', N'Employed'),
+(2, N'Maria', N'Nowak', N'Maid', N'ul. Rozana 2', N'Rzeszow', N'00-001', N'Polska', N'876321111   ', N'22222222222', N'Employed'),
+(3, N'Monika', N'Wichura', N'Maid', N'ul. Bratkowa 2', N'Krakow', N'00-002', N'Polska', N'876321122   ', N'33333333333', N'Employed'),
+(4, N'Lukasz', N'Biegun', N'Lifeguard', N'ul. Irysowa 2', N'Hel', N'00-003', N'Polska', N'876321133   ', N'44444444444', N'Employed'),
+(5, N'Jozef', N'Stachowski', N'Masseur/Masseuse', N'ul. Rzepakowa 2', N'Wroclaw', N'00-004', N'Polska', N'876321144   ', N'55555555555', N'Employed'),
+(6, N'Anna', N'Wozniak', N'Reception Manager', N'ul. Ziemniaczana 2', N'Wroclaw', N'00-005', N'Polska', N'876321155   ', N'66666666666', N'Employed'),
+(7, N'Krzysztof', N'Pak', N'Receptionist', N'ul. Marchewkowa 2', N'Wroclaw', N'00-006', N'Polska', N'876321166   ', N'77777777777', N'Employed')
+
+
 
 -- Guest types
 
@@ -107,16 +109,15 @@ SET IDENTITY_INSERT Guests ON
 
 INSERT INTO Guests (ID_Guest,FirstName,LastName,Address,City,PostCode,Country,PhoneNumber,EmailAddress,ID_GuestType,[Pesel/Passport],AdditionalInformation) 
 VALUES (1, N'Marzena', N'Piach', N'ul. Kwiatowa 124', N'Gdynia', N'00-123', N'Polska', N'987654321   ', N'marzp@guest.com', 5, N'12312312312', NULL),
-(2, N'Jerzy', N'Krak', N'ul. Basztowa 12', N'Gdañsk', N'00-123', N'Polska', N'983526421   ', N'jekr@guest.com', 5, N'32132132132', NULL),
-(3, N'Danuta', N'Oliwka', N'ul. Ró¿ana 124', N'Kraków', N'00-123', N'Polska', N'432134514   ', N'daol@guest.com', 1, N'23423423423', NULL),
-(4, N'Kamil', N'Dudek', N'ul. Makowa 124', N'Bia³ystok', N'00-123', N'Polska', N'911133333   ', N'kadu@guest.com', 1, N'45645645645', NULL),
+(2, N'Jerzy', N'Krak', N'ul. Basztowa 12', N'Gdansk', N'00-123', N'Polska', N'983526421   ', N'jekr@guest.com', 5, N'32132132132', NULL),
+(3, N'Danuta', N'Oliwka', N'ul. Rozana 124', N'Krakow', N'00-123', N'Polska', N'432134514   ', N'daol@guest.com', 1, N'23423423423', NULL),
+(4, N'Kamil', N'Dudek', N'ul. Makowa 124', N'Bialystok', N'00-123', N'Polska', N'911133333   ', N'kadu@guest.com', 1, N'45645645645', NULL),
 (5, N'John', N'Taylor', N'ul. 68 Reegans Road', N'Barkes Vale', N'NSW 2474', N'Australia', N'346234235   ', N'jota@guest.com', 3, N'AU 1562351 ', NULL),
-(6, N'Jadwiga', N'Oleszko', N'ul. Zielona 32', N'Przemyœl', N'00-123', N'Polska', N'123332211   ', N'jaol@guest.com', 4, N'90409437323', N'<Ankieta><Gosc><Imie>Jadwiga</Imie><Nazwisko>Oleszko</Nazwisko></Gosc><Informacje><Ocena><Wy¿ywienie>4</Wy¿ywienie><Rozrywki>4</Rozrywki><Rekreacja>5</Rekreacja><Czystoœæ>5</Czystoœæ><PotrzebyKlienta>4</PotrzebyKlienta><PracownicyObiektu>5</PracownicyObiektu><Cena>3</Cena></Ocena><Opis><Cel>Wypoczynek</Cel><Pochwa³a>Na pochwa³ê zas³uguje pokój nr 16. To mój ulubiony pokój. Widok z okna jest fenomenalny. </Pochwa³a><Poprawa>Mo¿ecie pomyœleæ o dodaniu opcji dañ wegañskich w menu restauracji.</Poprawa></Opis></Informacje></Ankieta>'),
-(7, N'Jan', N'Sikora', N'ul. Morska 4', N'Zielona Góra', N'00-123', N'Polska', N'999888777   ', N'jasi@guest.com', 2, N'77351221365', NULL),
-(8, N'Jolanta', N'Zych', N'ul. Nagietkowa 14', N'Wroc³aw', N'12-432', N'Polska', N'652123123   ', NULL, 5, N'12376317643', NULL),
-(10, N'Marian', N'Oleksy', N'ul. Koœciuszki 21', N'Nowy Targ', N'11-111', N'Polska', N'982664123   ', N'maol@guest.com', 5, N'76312312312', NULL)
+(6, N'Jadwiga', N'Oleszko', N'ul. Zielona 32', N'Przemysl', N'00-123', N'Polska', N'123332211   ', N'jaol@guest.com', 4, N'90409437323', N'<Survey><Guest><FName>Jadwiga</FName><LName>Oleszko</LName></Guest><Information><Grade><Catering>4</Catering><Entertainment>4</Entertainment><Recreation>5</Recreation><Cleanness>5</Cleanness><CustomerNeeds>4</CustomerNeeds><Staff>5</Staff><Price>3</Price></Grade><Description><Aim>Wypoczynek</Aim><Praise>Na pochwaÅ‚Ä™ zasÅ‚uguje pokÃ³j nr 16. To mÃ³j ulubiony pokÃ³j. Widok z okna jest fenomenalny. </Praise><ToCorrection>MoÅ¼ecie pomyÅ›leÄ‡ o dodaniu opcji daÅ„ wegaÅ„skich w menu restauracji.</ToCorrection></Description></Information></Survey>'),
+(7, N'Jan', N'Sikora', N'ul. Morska 4', N'Zielona Gora', N'00-123', N'Polska', N'999888777   ', N'jasi@guest.com', 2, N'77351221365', NULL),
+(8, N'Jolanta', N'Zych', N'ul. Nagietkowa 14', N'Wroclaw', N'12-432', N'Polska', N'652123123   ', NULL, 5, N'12376317643', NULL),
+(10, N'Marian', N'Oleksy', N'ul. Kosciuszki 21', N'Nowy Targ', N'11-111', N'Polska', N'982664123   ', N'maol@guest.com', 5, N'76312312312', NULL)
 SET IDENTITY_INSERT Guests OFF
-
 
 -- Room statuses
 
