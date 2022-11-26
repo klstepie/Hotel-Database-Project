72cd5cbcf7fba77cb7991e6120053c8f0a148de7
klstepie <107317111+klstepie@users.noreply.github.com>
2022-11-26 21:44:54 +0100

    Update hotel_procedures.sql

--- hotel_procedures.sql
+++ hotel_procedures.sql
@@ -1,3 +1,65 @@
+-- Adding employees
+
+CREATE PROCEDURE p_AddEmployee
+@id_employee int,
+@firstname varchar(20),
+@lastname varchar(20),
+@position varchar(30), 
+@address varchar(30),
+@city varchar(20),
+@postcode varchar(10),
+@country varchar(20),
+@phone char(9),
+@pesel char(11),
+@deptno char(3)
+AS
+BEGIN
+IF(@id_employee IN (SELECT ID_Employee FROM Employees))
+BEGIN
+RAISERROR ('The employee ID number already exists in the database.',16,1)
+END
+ELSE
+BEGIN
+IF(@pesel IN (SELECT PESEL FROM Employees))
+BEGIN
+RAISERROR ('PESEL number already exists in the database.',16,1)
+END
+ELSE
+BEGIN
+INSERT INTO Employees
+(
+ID_Employee, 
+FirstName,
+LastName,
+Position, 
+Address, 
+City, 
+Postcode, 
+Country,
+PhoneNumber, 
+PESEL,
+DeptNo
+)
+VALUES (
+@id_employee, 
+@firstname,
+@lastname,
+@position,
+@address,
+@city,
+@postcode,
+@country,
+@phone,
+@pesel,
+@deptno
+)
+END
+END
+END
+GO
+
+-- Promoting employees
+
 CREATE PROCEDURE p_PromoteEmployee
 @Id_Employee int,
 @Position varchar(30)
@@ -16,6 +78,7 @@ RAISERROR ('Incorrect employee identification number.',16,1)
 END
 GO
 
+-- Adding guests
 
 CREATE PROCEDURE p_AddGuest
 @firstname varchar(20),
@@ -62,3 +125,37 @@ BEGIN
 RAISERROR ('The guest with this document number already exists in the database.',16,1)
 END
 GO
+
+-- Adding rooms to reservations
+
+CREATE PROCEDURE p_AddRoomToReservation
+@id_booking int,
+@id_room int
+AS
+IF(@id_booking IN (SELECT ID_Booking FROM Bookings) AND @id_room IN (SELECT ID_Room FROM Rooms))
+	BEGIN
+	IF(@id_room IN (SELECT ID_Room FROM BookingRooms WHERE ID_Booking = @id_booking))
+		BEGIN
+		-- We can't add two rooms with the same number.
+		RAISERROR ('The room ID number is already added to this booking.',16,1)
+		END
+		ELSE
+		BEGIN
+		INSERT INTO BookingRooms
+		(
+		ID_Booking,
+		ID_Room
+		)
+		VALUES
+		(
+		@id_booking,
+		@id_room
+		)
+		END
+	END
+	ELSE
+	BEGIN
+	RAISERROR ('Incorrect booking/room identification number.',16,1)
+	END
+GO
+
