55237206fe58305c2006187429c34fa89c2b6472
klstepie <107317111+klstepie@users.noreply.github.com>
2022-11-28 22:15:27 +0100

    Update hotel_procedures.sql

--- hotel_procedures.sql
+++ hotel_procedures.sql
@@ -159,3 +159,206 @@ IF(@id_booking IN (SELECT ID_Booking FROM Bookings) AND @id_room IN (SELECT ID_R
 	END
 GO
 
+-- Adding reservation
+
+CREATE PROCEDURE p_AddBooking
+@id_guest int, 
+@datefrom smalldatetime, 
+@dateto smalldatetime, 
+@numofrooms int, 
+@numofguests int 
+AS
+SET NOCOUNT ON 
+IF(@id_guest IN (SELECT ID_Guest FROM Guests))
+BEGIN
+	IF(@datefrom < @dateto AND @datefrom != @dateto)
+	BEGIN
+	DECLARE @status int = 2
+	DECLARE @dateofbooking smalldatetime = GETDATE()
+	INSERT INTO Bookings
+	(
+	ID_Guest, 
+	DateFrom,
+	DateTo,
+	NumberOfRooms,
+	ID_BookingStatus,
+	BookingDate,
+	NumberOfGuests
+	)
+	VALUES (
+	@id_guest,
+	@datefrom,
+	@dateto,
+	@numofrooms,
+	@status,
+	@dateofbooking,
+	@numofguests
+	)
+
+	SELECT 
+	ID_Booking, 
+	G.FirstName,
+	G.LastName,
+	DateFrom,
+	DateTo,
+	NumberOfRooms,
+	NumberOfGuests
+	FROM Bookings B
+	JOIN Guests G
+	ON B.ID_Guest = G.ID_Guest
+	WHERE B.ID_Guest = @id_guest
+
+	END
+	ELSE
+	BEGIN
+	RAISERROR ('Wrong date is entered.',16,1)
+	END
+END
+ELSE
+BEGIN
+RAISERROR ('There is no such guest.',16,1)
+END
+GO
+
+-- Generating summary
+
+CREATE procedure p_GenerateSummaryOfServices
+@id_booking int
+AS
+BEGIN
+	IF(@id_booking IN (SELECT ID_Booking FROM Bookings))
+		BEGIN
+		SELECT B.ID_Booking, RT.Type AS 'Service name', B.DateFrom AS 'Start date of service', 
+		B.DateTo AS 'End date of service', 
+		DATEDIFF(DAY,B.DateFrom,B.DateTo) AS 'Quantity','D' AS 'Unit',
+		RT.Price AS 'Gross unit price',RT.Price * (DATEDIFF(DAY,B.DateFrom,B.DateTo)) AS 'Gross amount'
+		INTO #TempSummaryOfStay
+		FROM Bookings B
+		JOIN BookingRooms BR
+		ON B.ID_Booking = BR.ID_Booking
+		JOIN Rooms R
+		ON R.ID_Room = BR.ID_Room
+		JOIN RoomTypes RT
+		ON RT.ID_RoomType = R.ID_RoomType
+
+
+		SELECT B.ID_Booking, HS.Name AS 'Service name', RS.StartDate AS 'Start date of service', 
+		RS.EndDate AS 'End date of service', 
+		CASE
+		WHEN HS.TimeUnit = 'H' THEN DATEDIFF(HOUR,RS.StartDate,RS.EndDate) 
+		WHEN HS.TimeUnit = 'D' THEN DATEDIFF(DAY,RS.StartDate,RS.EndDate) 
+		END AS 'Quantity',
+		HS.TimeUnit AS 'Unit',
+		HS.Price AS 'Gross unit price',
+		CASE
+		WHEN HS.TimeUnit = 'H' THEN HS.Price * DATEDIFF(HOUR,RS.StartDate,RS.EndDate) 
+		WHEN HS.TimeUnit = 'D' THEN HS.Price * DATEDIFF(DAY,RS.StartDate,RS.EndDate) 
+		END AS 'Gross amount'
+		INTO #TempSummaryOfServices
+		FROM Bookings B
+		JOIN ReservationOfServices RS
+		ON RS.ID_Booking = B.ID_Booking
+		JOIN HotelServices HS
+		ON HS.ID_Service = RS.ID_Service
+
+		Create Table #FinalStatement(
+		ID_Booking int,
+		[Name of service/room] varchar(50),
+		[Start date of service] smalldatetime,
+		[End date of service] smalldatetime,
+		[Quantity] INT,
+		[Unit] CHAR(1),
+		[Gross unit price] FLOAT,
+		[Gross amount] FLOAT
+
+
+		)
+
+		INSERT INTO #FinalStatement
+		SELECT *
+		FROM #TempSummaryOfServices
+		UNION ALL
+		SELECT *
+		FROM #TempSummaryOfStay
+
+		SELECT *
+		FROM #FinalStatement
+		WHERE ID_Booking = @id_booking
+
+		DROP table #TempSummaryOfStay
+		DROP table #TempSummaryOfServices
+		DROP table #FinalStatement
+		END
+	ELSE
+	BEGIN
+	RAISERROR ('Invalid booking number.',16,1)
+	END
+
+END
+GO
+
+-- Check in
+
+CREATE PROCEDURE p_CheckIn
+@id_booking int,
+@id_room int,
+@status int = 2
+AS
+DECLARE @datefrom date;
+DECLARE @dateto date;
+DECLARE @id_roombook int;
+
+
+IF(@id_booking IN (SELECT ID_Booking FROM Bookings))
+BEGIN
+	SET @id_roombook = (SELECT ID_BookingRoom
+	FROM BookingRooms
+	WHERE ID_Room = @id_room AND ID_Booking = @id_booking)
+	IF(@id_roombook IN (SELECT ID_BookingRoom FROM BookingRooms))
+	BEGIN
+	
+		IF(@status = 2 OR @status = 4)
+		BEGIN
+        set @datefrom = (
+			SELECT DateFrom
+		FROM Bookings
+			WHERE ID_Booking = @id_booking
+		)
+
+		set @dateto = (
+			SELECT DateTo
+			FROM Bookings
+			WHERE ID_Booking = @id_booking
+			)
+
+		IF (GETDATE() BETWEEN @datefrom AND @dateto)
+				  BEGIN
+					UPDATE Rooms
+					SET ID_RoomState = @status
+				WHERE ID_Room = @id_room
+				 END
+				  ELSE
+				 BEGIN
+				RAISERROR ('The guest arrived not on the date he had booked.',16,1)
+			END
+			END
+			ELSE
+			BEGIN
+			RAISERROR ('Incorrect status number.',16,1)
+			END
+	END
+	ELSE
+	BEGIN
+	RAISERROR ('Incorrect room number.',16,1)
+	END
+END
+ELSE
+BEGIN
+RAISERROR ('Incorrect booking number.',16,1)
+END
+GO
+
+
+
+
+
