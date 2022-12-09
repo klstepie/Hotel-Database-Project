723ed5cf59032cc1c68ee4bf6b60d78e815c693d
klstepie <107317111+klstepie@users.noreply.github.com>
2022-12-09 23:44:09 +0100

    Update hotel_functions.sql

--- hotel_functions.sql
+++ hotel_functions.sql
@@ -14,3 +14,24 @@ DECLARE @number int
 SET @number = dbo.checkrate(21)
 SELECT @number AS NumberOfEmployees
 
+-- Displaying and counting all of the employee's payments 
+
+CREATE FUNCTION payment_table(@id_employee int)
+RETURNS @PaymentEmployee TABLE (Id_Employee int, FirstName varchar(20),
+LastName varchar(20), Position varchar(30), DateYear smallint, DateMonth smallint, TotalPayment float)
+AS
+BEGIN
+INSERT INTO @PaymentEmployee 
+SELECT E.ID_Employee, FirstName,LastName,Position,YEAR(EH.Date), MONTH(EH.Date), SUM(WorkHours) * Rate 
+FROM Employees E
+JOIN EmployeeHoursPerDay EH
+ON E.ID_Employee = EH.ID_Employee
+JOIN  EmployeePayments AS EP
+ON EH.ID_Employee = EP.ID_Employee
+WHERE E.ID_Employee = @id_employee
+GROUP BY E.ID_Employee,FirstName,LastName,Position, YEAR(Date), MONTH(Date), Rate
+RETURN
+END
+
+-- Execute
+SELECT * FROM dbo.payment_table(1)
